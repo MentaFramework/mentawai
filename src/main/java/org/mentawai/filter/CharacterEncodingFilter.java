41e1cf269ca9ebfc17777308c23f575e8f28e39c
helio.frota <helio.frota@739d1767-7221-4712-816e-538b8844ec24>
2011-12-21 01:28:49 +0000

    tabs to spaces..
    git-svn-id: svn://soliveirajr.com/mentawai/trunk@171 739d1767-7221-4712-816e-538b8844ec24

--- src/main/java/org/mentawai/filter/CharacterEncodingFilter.java
+++ src/main/java/org/mentawai/filter/CharacterEncodingFilter.java
@@ -26,90 +26,90 @@ import org.mentawai.core.InvocationChain;
 import org.mentawai.core.SessionContext;
 
 /**
- * Filter class CharacterEncodingFilter can change charset and <br> 
+ * Filter class CharacterEncodingFilter can change charset and <br>
  * content type system wide.
- * 
+ *
  * @author helio frota
  *
  */
 public class CharacterEncodingFilter implements Filter {
-	
-	/**
-	 * Attribute constant UTF_8 of CharacterEncodingFilter.
-	 */
-	public static final String UTF_8 = "UTF-8";
-	/**
-	 * Attribute constant ISO_8859_1 of CharacterEncodingFilter.
-	 */
-	public static final String ISO_8859_1 = "ISO-8859-1";
-	/**
-	 * Attribute constant TEXT_PLAIN of CharacterEncodingFilter.
-	 */
-	public static final String TEXT_PLAIN = "text/plain";
-	/**
-	 * Attribute constant TEXT_HTML of CharacterEncodingFilter.
-	 */
-	public static final String TEXT_HTML = "text/html";
-	/**
-	 * Attribute constant TEXT_CSS of CharacterEncodingFilter.
-	 */
-	public static final String TEXT_CSS = "text/css";
-	
-	/**
-	 * Attribute charSet of CharacterEncodingFilter.
-	 */
-	private String charSet = "UTF-8";
-	/**
-	 * Attribute contentType of CharacterEncodingFilter.
-	 */
-	private String contentType = "text/html";
-	
-	/**
-	 * Default constructor.
-	 */
-	public CharacterEncodingFilter() {
-		
-	}
-	
-	/**
-	 * Parametric constructor.
-	 * @param charSet String
-	 */
-    public CharacterEncodingFilter(String charSet) {
-		this.charSet = charSet;
-	}
-    
+
     /**
-	 * Parametric constructor.
-	 * @param charSet String
-	 */
+     * Attribute constant UTF_8 of CharacterEncodingFilter.
+     */
+    public static final String UTF_8 = "UTF-8";
+    /**
+     * Attribute constant ISO_8859_1 of CharacterEncodingFilter.
+     */
+    public static final String ISO_8859_1 = "ISO-8859-1";
+    /**
+     * Attribute constant TEXT_PLAIN of CharacterEncodingFilter.
+     */
+    public static final String TEXT_PLAIN = "text/plain";
+    /**
+     * Attribute constant TEXT_HTML of CharacterEncodingFilter.
+     */
+    public static final String TEXT_HTML = "text/html";
+    /**
+     * Attribute constant TEXT_CSS of CharacterEncodingFilter.
+     */
+    public static final String TEXT_CSS = "text/css";
+
+    /**
+     * Attribute charSet of CharacterEncodingFilter.
+     */
+    private String charSet = "UTF-8";
+    /**
+     * Attribute contentType of CharacterEncodingFilter.
+     */
+    private String contentType = "text/html";
+
+    /**
+     * Default constructor.
+     */
+    public CharacterEncodingFilter() {
+
+    }
+
+    /**
+     * Parametric constructor.
+     * @param charSet String
+     */
+    public CharacterEncodingFilter(String charSet) {
+        this.charSet = charSet;
+    }
+
+    /**
+     * Parametric constructor.
+     * @param charSet String
+     */
     public CharacterEncodingFilter(String charSet, String contentType) {
-		this.charSet = charSet;
-		this.contentType = contentType;
-	}
-    
-	/**
-	 * Default filter method.
-	 * @param InvocationChain chain
-	 */
-	public String filter(InvocationChain chain) throws Exception {
+        this.charSet = charSet;
+        this.contentType = contentType;
+    }
 
-		Action action = chain.getAction();
-		
-		HttpServletResponse httpServletResponse = ((SessionContext) action.getSession()).getResponse();
-		
-		httpServletResponse.setContentType(contentType + "; charset=" + charSet);
-		
-		httpServletResponse.setCharacterEncoding(charSet);
-		
-		return chain.invoke();
-	}
+    /**
+     * Default filter method.
+     * @param InvocationChain chain
+     */
+    public String filter(InvocationChain chain) throws Exception {
 
-	/**
-	 * Default destroy method.
-	 */
-	public void destroy() {
+        Action action = chain.getAction();
 
-	}
-	
+        HttpServletResponse httpServletResponse = ((SessionContext) action.getSession()).getResponse();
+
+        httpServletResponse.setContentType(contentType + "; charset=" + charSet);
+
+        httpServletResponse.setCharacterEncoding(charSet);
+
+        return chain.invoke();
+    }
+
+    /**
+     * Default destroy method.
+     */
+    public void destroy() {
+
+    }
+
 }
