74339ff0a9fa8468d5488b764903d2eb6a9b40c3
helio.frota <helio.frota@739d1767-7221-4712-816e-538b8844ec24>
2012-02-22 11:51:43 +0000

    fixing the trailing spaces.
    git-svn-id: svn://soliveirajr.com/mentawai/trunk@197 739d1767-7221-4712-816e-538b8844ec24

--- src/main/java/org/mentawai/filter/ExceptionFilter.java
+++ src/main/java/org/mentawai/filter/ExceptionFilter.java
@@ -56,158 +56,158 @@ import org.mentawai.core.Output;
  */
 public class ExceptionFilter implements Filter {
 
-	/** Attribute MESSAGE_KEY of ExceptionFilter. */
-	public static String MESSAGE_KEY = "message";
+    /** Attribute MESSAGE_KEY of ExceptionFilter. */
+    public static String MESSAGE_KEY = "message";
 
-	/** Attribute EXCEPTION_KEY of ExceptionFilter. */
-	public static String EXCEPTION_KEY = "exception";
+    /** Attribute EXCEPTION_KEY of ExceptionFilter. */
+    public static String EXCEPTION_KEY = "exception";
 
-	/** Attribute STACK_TRACE_KEY of ExceptionFilter. */
-	public static String STACK_TRACE_KEY = "stacktrace";
+    /** Attribute STACK_TRACE_KEY of ExceptionFilter. */
+    public static String STACK_TRACE_KEY = "stacktrace";
 
-	/** Attribute STACK_HEADER_KEY of ExceptionFilter. */
-	public static String STACK_HEADER_KEY = "stackheader";
+    /** Attribute STACK_HEADER_KEY of ExceptionFilter. */
+    public static String STACK_HEADER_KEY = "stackheader";
 
-	/** Attribute EXCEPTION of ExceptionFilter. */
-	public static String EXCEPTION = "exception";
+    /** Attribute EXCEPTION of ExceptionFilter. */
+    public static String EXCEPTION = "exception";
 
-	/** Attribute trace of ExceptionFilter. */
-	private boolean trace = true;
+    /** Attribute trace of ExceptionFilter. */
+    private boolean trace = true;
 
-	/**
-	 * Default constructor.
-	 */
-	public ExceptionFilter() {
+    /**
+     * Default constructor.
+     */
+    public ExceptionFilter() {
 
-	}
+    }
 
-	/**
-	 * Parametric constructor.
-	 * @param trace boolean
-	 */
-	public ExceptionFilter(boolean trace) {
-		this.trace = trace;
-	}
+    /**
+     * Parametric constructor.
+     * @param trace boolean
+     */
+    public ExceptionFilter(boolean trace) {
+        this.trace = trace;
+    }
 
-	/**
-	 * Execute the chain and cacth any exception that occours, delegating to the
-	 * handleException() method the resposability to handle the exception
-	 * throwed by the chain's execution.
-	 *
-	 * @return the result of the action's execution.
-	 */
-	public String filter(InvocationChain chain) throws Exception {
-		try {
-			return chain.invoke();
-		} catch (Throwable throwable) {
-			return handleException(chain.getAction(), throwable);
-		}
-	}
+    /**
+     * Execute the chain and cacth any exception that occours, delegating to the
+     * handleException() method the resposability to handle the exception
+     * throwed by the chain's execution.
+     *
+     * @return the result of the action's execution.
+     */
+    public String filter(InvocationChain chain) throws Exception {
+        try {
+            return chain.invoke();
+        } catch (Throwable throwable) {
+            return handleException(chain.getAction(), throwable);
+        }
+    }
 
-	/**
-	 * Gets the root cause of exception.
-	 * @param throwable Throwable
-	 * @return Throwable
-	 */
-	protected static Throwable getRootCause(Throwable throwable) {
-		Deque < Throwable > list = new LinkedList < Throwable >();
+    /**
+     * Gets the root cause of exception.
+     * @param throwable Throwable
+     * @return Throwable
+     */
+    protected static Throwable getRootCause(Throwable throwable) {
+        Deque < Throwable > list = new LinkedList < Throwable >();
         while (throwable != null && list.contains(throwable) == false) {
             list.add(throwable);
             throwable = throwable.getCause();
         }
-		return list.getLast();
-	}
+        return list.getLast();
+    }
 
-	/**
-	 * Handle the exception, putting in the action's output the message of the
-	 * exception, all the StackTrace elements and the first StackTrace element.
-	 * <br/> This method can be overrided to change the behavior of the
-	 * ExceptionFilter.
-	 *
-	 * @param a the action that has throwed the exception.
-	 * @param throwable the exception throwed by the action's execution.
-	 * @return the result of the action's execution.
-	 */
-	protected String handleException(Action a, Throwable throwable) {
+    /**
+     * Handle the exception, putting in the action's output the message of the
+     * exception, all the StackTrace elements and the first StackTrace element.
+     * <br/> This method can be overrided to change the behavior of the
+     * ExceptionFilter.
+     *
+     * @param a the action that has throwed the exception.
+     * @param throwable the exception throwed by the action's execution.
+     * @return the result of the action's execution.
+     */
+    protected String handleException(Action a, Throwable throwable) {
 
-		Throwable t = getRootCause(throwable);
+        Throwable t = getRootCause(throwable);
 
-		String exp = t != null ? t.getClass().getName() : throwable.getClass().getName();
+        String exp = t != null ? t.getClass().getName() : throwable.getClass().getName();
 
-		Output output = a.getOutput();
-		output.setValue(EXCEPTION_KEY, exp);
-		output.setValue(MESSAGE_KEY, t.getMessage());
+        Output output = a.getOutput();
+        output.setValue(EXCEPTION_KEY, exp);
+        output.setValue(MESSAGE_KEY, t.getMessage());
 
-		StringWriter sw = new StringWriter();
+        StringWriter sw = new StringWriter();
 
-		PrintWriter pw = new PrintWriter(sw, true);
+        PrintWriter pw = new PrintWriter(sw, true);
 
-		t.printStackTrace(pw);
+        t.printStackTrace(pw);
 
-	    if (t != null) {
+        if (t != null) {
 
-	    	pw.println(sw.getBuffer().toString());
+            pw.println(sw.getBuffer().toString());
 
-	    	pw.flush();
+            pw.flush();
 
-	    }
+        }
 
-	    String full_trace = sw.getBuffer().toString();
+        String full_trace = sw.getBuffer().toString();
 
-	    String[] lines = full_trace.split("\\n");
+        String[] lines = full_trace.split("\\n");
 
-	    output.setValue(STACK_HEADER_KEY, lines[0]);
+        output.setValue(STACK_HEADER_KEY, lines[0]);
 
-	    output.setValue(STACK_TRACE_KEY, prepareStackTrace(lines));
+        output.setValue(STACK_TRACE_KEY, prepareStackTrace(lines));
 
-		if (trace) {
+        if (trace) {
 
-		    if (t != null) {
+            if (t != null) {
 
-		    	System.err.println(sw.getBuffer().toString());
+                System.err.println(sw.getBuffer().toString());
 
-		    	System.err.flush();
+                System.err.flush();
 
-		    }
+            }
 
-		}
+        }
 
-		return EXCEPTION;
-	}
+        return EXCEPTION;
+    }
 
-	protected String prepareStackTrace(String[] stacktrace) {
-		StringBuffer sb = new StringBuffer(stacktrace.length * 75);
-		for (int i = 0; i < stacktrace.length; i++) {
-			String s = stacktrace[i];
-			if (i != 0)
-				sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(prepareForHtml(s)).append("<br/>");
-		}
-		return sb.toString();
-	}
+    protected String prepareStackTrace(String[] stacktrace) {
+        StringBuffer sb = new StringBuffer(stacktrace.length * 75);
+        for (int i = 0; i < stacktrace.length; i++) {
+            String s = stacktrace[i];
+            if (i != 0)
+                sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+            sb.append(prepareForHtml(s)).append("<br/>");
+        }
+        return sb.toString();
+    }
 
-	protected String prepareStackTrace(StackTraceElement[] stacktrace) {
-		StringBuffer sb = new StringBuffer(stacktrace.length * 75);
-		for (int i = 0; i < stacktrace.length; i++) {
-			String s = stacktrace[i].toString();
-			if (i != 0)
-				sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(prepareForHtml(s)).append("<br/>");
-		}
-		return sb.toString();
-	}
+    protected String prepareStackTrace(StackTraceElement[] stacktrace) {
+        StringBuffer sb = new StringBuffer(stacktrace.length * 75);
+        for (int i = 0; i < stacktrace.length; i++) {
+            String s = stacktrace[i].toString();
+            if (i != 0)
+                sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+            sb.append(prepareForHtml(s)).append("<br/>");
+        }
+        return sb.toString();
+    }
 
-	protected String prepareForHtml(String s) {
-		s = s.replaceAll("<", "&lt;");
-		s = s.replaceAll(">", "&gt;");
-		return s;
-	}
+    protected String prepareForHtml(String s) {
+        s = s.replaceAll("<", "&lt;");
+        s = s.replaceAll(">", "&gt;");
+        return s;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void destroy() {
+    /**
+     * {@inheritDoc}
+     */
+    public void destroy() {
 
-	}
+    }
 
 }
