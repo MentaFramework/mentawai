2fc94543959b795cccc3eccd8557b143b5bc4d2e
robertwgil <robertwgil@739d1767-7221-4712-816e-538b8844ec24>
2014-01-06 12:07:04 +0000

    continuar com o mesmo protocolo utilizado para efetuar o login.
    git-svn-id: svn://soliveirajr.com/mentawai/trunk@332 739d1767-7221-4712-816e-538b8844ec24

--- src/main/java/org/mentawai/filter/RedirectAfterLoginFilter.java
+++ src/main/java/org/mentawai/filter/RedirectAfterLoginFilter.java
@@ -18,51 +18,81 @@
  */
 package org.mentawai.filter;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.mentawai.action.BaseLoginAction;
 import org.mentawai.core.Action;
 import org.mentawai.core.BaseAction;
 import org.mentawai.core.Context;
 import org.mentawai.core.Filter;
+import org.mentawai.core.Input;
 import org.mentawai.core.InvocationChain;
 import org.mentawai.core.Output;
 import org.mentawai.core.Redirect;
 
 /**
- * A filter that implements the redirect after login mechanism.
- * Apply this filter to your Login action if you want it to perform a redict 
- * to the first page the user tried to access.
- *
+ * A filter that implements the redirect after login mechanism. Apply this
+ * filter to your Login action if you want it to perform a redict to the first
+ * page the user tried to access.
+ * 
  * @author Sergio Oliveira
  */
 public class RedirectAfterLoginFilter implements Filter {
-	
-    /**
-     * The result indicating that a redict after login should be done.
-     */
+
+	/**
+	 * The result indicating that a redict after login should be done.
+	 */
 	public static final String REDIR = "redir";
-	
-    /**
-     * Creates a RedirectAfterLoginFilter.
-     */
-	public RedirectAfterLoginFilter() { }
-	
+
+	/**
+	 * Creates a RedirectAfterLoginFilter.
+	 */
+	public RedirectAfterLoginFilter() {
+	}
+
 	public String filter(InvocationChain chain) throws Exception {
 		Action action = chain.getAction();
 		if (action instanceof BaseLoginAction) {
-    		Context session = action.getSession();
-    		String callback = (String) session.getAttribute(AuthenticationFilter.URL_KEY);
-    		String result = chain.invoke();
-    		if (callback != null && !result.equals(BaseAction.ERROR)) {
-    			Output output = action.getOutput();
-    			output.setValue(Redirect.REDIRURL_PARAM, callback);
-    			return REDIR;
-    		}
-    		return result;
+			Context session = action.getSession();
+			String callback = (String) session.getAttribute(AuthenticationFilter.URL_KEY);
+			String result = chain.invoke();
+			if (callback != null && !result.equals(BaseAction.ERROR)) {
+				Output output = action.getOutput();
+				Input input = action.getInput();
+				output.setValue(Redirect.REDIRURL_PARAM, changeProtocolUrl(input.getProperty("requestURL"), callback));
+				return REDIR;
+			}
+			return result;
 		} else {
 			return chain.invoke();
 		}
 	}
-    
-    public void destroy() { }
+
+	/**
+	 * The redirect to after login should be done with the same protocol
+	 * 
+	 * @param urlInput
+	 * @param urlCallback
+	 * @return
+	 */
+	private String changeProtocolUrl(String urlInput, String urlCallback) {
+		String urlResult = urlCallback;
+
+		try {
+			URL urlRequest = new URL(urlInput);
+			URL urlSession = new URL(urlCallback);
+
+			if (urlRequest.getHost().equals(urlSession.getHost()) && !urlRequest.getProtocol().equals(urlSession.getProtocol())) {
+				urlResult = urlCallback.replace(urlSession.getProtocol() + "://", urlRequest.getProtocol() + "://");
+			}
+		} catch (MalformedURLException e) {
+			urlResult = urlCallback;
+		}
+
+		return urlResult;
+	}
+
+	public void destroy() {
+	}
 }
-		
