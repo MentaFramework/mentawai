23b1da52f98f434edf1752a1880ce3181f514683
robertwgil <robertwgil@739d1767-7221-4712-816e-538b8844ec24>
2014-11-06 13:34:10 +0000

    dont make cache of Cipher on DesEncrypter.
    git-svn-id: svn://soliveirajr.com/mentawai/trunk@334 739d1767-7221-4712-816e-538b8844ec24

--- src/main/java/org/mentawai/cript/DesEncrypter.java
+++ src/main/java/org/mentawai/cript/DesEncrypter.java
@@ -3,11 +3,16 @@ package org.mentawai.cript;
 import static org.mentalog.Log.Debug;
 
 import java.io.UnsupportedEncodingException;
-import java.security.spec.AlgorithmParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.logging.Level;
 
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -17,44 +22,40 @@ import com.thoughtworks.xstream.core.util.Base64Encoder;
 
 public class DesEncrypter {
 
-	private Cipher ecipher;
-	private Cipher dcipher;
-
 	// 8-byte Salt
 	private byte[] salt = { (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32, (byte) 0x56, (byte) 0x35, (byte) 0xE3, (byte) 0x03 };
 
 	// Iteration count
 	private final int iterationCount = 2;
 
+	private SecretKey key;
+	private PBEParameterSpec paramSpec;
+	
 	public DesEncrypter(String passPhrase) {
+		
+		// Create the key
 		try {
-			// Create the key
-			KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(), salt, iterationCount);
-			SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-			ecipher = Cipher.getInstance(key.getAlgorithm());
-			dcipher = Cipher.getInstance(key.getAlgorithm());
-
+			KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray());
+			key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
 			// Prepare the parameter to the ciphers
-			AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
+			paramSpec = new PBEParameterSpec(salt, iterationCount);
 
-			// Create the ciphers
-			ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-			dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-		} catch (java.security.InvalidAlgorithmParameterException e) {
+		} catch (InvalidKeySpecException e) {
 			Debug.log("Error creating component DesEncrypter:", e);
-		} catch (java.security.spec.InvalidKeySpecException e) {
-			Debug.log("Error creating component DesEncrypter:", e);
-		} catch (javax.crypto.NoSuchPaddingException e) {
-			Debug.log("Error creating component DesEncrypter:", e);
-		} catch (java.security.NoSuchAlgorithmException e) {
-			Debug.log("Error creating component DesEncrypter:", e);
-		} catch (java.security.InvalidKeyException e) {
+		} catch (NoSuchAlgorithmException e) {
 			Debug.log("Error creating component DesEncrypter:", e);
 		}
+				
 	}
 
 	public String encrypt(String str) {
 		try {
+			
+			// Create the ciphers
+			Cipher ecipher = Cipher.getInstance(key.getAlgorithm());
+			ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+
+			
 			// Encode the string into bytes using utf-8
 			byte[] utf8 = str.getBytes("UTF8");
 
@@ -63,22 +64,37 @@ public class DesEncrypter {
 
 			// Encode bytes to base64 to get a string
 			return new Base64Encoder().encode(enc);
+			
 		} catch (javax.crypto.BadPaddingException e) {
-			Debug.log("Error encrypting string: " + str, e);
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
 		} catch (IllegalBlockSizeException e) {
-			Debug.log("Error encrypting string: " + str, e);
+			Debug.log(Level.FINE, "Error decrypting string: " + str, e);
 		} catch (UnsupportedEncodingException e) {
-			Debug.log("Error encrypting string: " + str, e);
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (NoSuchAlgorithmException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (NoSuchPaddingException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (InvalidKeyException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (InvalidAlgorithmParameterException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (ArrayIndexOutOfBoundsException e){
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
 		}
+		
 		return null;
 	}
 
 	public String decrypt(String str) {
 		
 		try {
+			
+			Cipher dcipher = Cipher.getInstance(key.getAlgorithm());
+			dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
 			// Decode base64 to get bytes
 			byte[] dec = new Base64Encoder().decode(str);
-			
+
 			// Decrypt
 			byte[] utf8 = dcipher.doFinal(dec);
 
@@ -86,12 +102,22 @@ public class DesEncrypter {
 			return new String(utf8, "UTF8");
 			
 		} catch (javax.crypto.BadPaddingException e) {
-			Debug.log("Error decrypting string: " + str, e);
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
 		} catch (IllegalBlockSizeException e) {
-			Debug.log("Error decrypting string: " + str, e);
+			Debug.log(Level.FINE, "Error decrypting string: " + str, e);
 		} catch (UnsupportedEncodingException e) {
-			Debug.log("Error decrypting string: " + str, e);
-		} 
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (NoSuchAlgorithmException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (NoSuchPaddingException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (InvalidKeyException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (InvalidAlgorithmParameterException e) {
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		} catch (ArrayIndexOutOfBoundsException e){
+			Debug.log(Level.SEVERE, "Error decrypting string: " + str, e);
+		}
 		
 		return null;
 	}
